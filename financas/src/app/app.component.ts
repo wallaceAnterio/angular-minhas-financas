73fec317d2d3f1dd12c8c8e7ed7337cea5431cc9
RocketBulls <bullsrocketgit@gmail.com>
2021-09-05 20:03:26 -0300

    adicionando configuracao base

---
+++ financas/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'financas';
+
+  TestBed(){
+
+    console.log('teste')
+  }
+}
