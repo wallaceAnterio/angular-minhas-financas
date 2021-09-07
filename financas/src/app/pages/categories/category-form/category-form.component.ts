6ffbcb49b1a2877ba6be82c7164f66397032eafc
wallaceAnterio <wallacesanterio@gmail.com>
2021-09-07 00:05:40 -0300

    implementando metodos e verificacoes no category-form.component

--- financas/src/app/pages/categories/category-form/category-form.component.ts
+++ financas/src/app/pages/categories/category-form/category-form.component.ts
@@ -43,14 +43,28 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   ngAfterContentChecked(): void {
     this.setPageTitle();
   }
+
+  // Método responsǘel por enviar o formulário
+  submitForm() {
+    this.submittingForm = true; // desbloquei o botão, ao enviar o formulário
+
+    if (this.currentAction == "new") {
+      this.createCategory();
+    }
+    else {
+      this.currentAction = "edit"
+      this.updateCategory();
+    }
+  }
+
   // MÈTODOS PRIVADOS
   // Define a ação atual do formulário
   private setCurrentAction() {
-    if (this.activatRoute.snapshot.url[0].path == 'new') {
-      this.currentAction == 'new'
+    if (this.activatRoute.snapshot.url[0].path == "new") {
+      this.currentAction = "new"
     }
     else {
-      this.currentAction = 'edit'
+      this.currentAction = "edit"
     }
   }
 
@@ -68,25 +82,69 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
       this.activatRoute.paramMap.pipe(
         switchMap(params => this.categoryService.getById(+params.get('id')))
+
       )
-      .subscribe((category) => {
-        this.category = category
-        this.categoryForm.patchValue(category) // setando os valores da categoria, para o formulário
-      }, (error) => alert('Ocorreu um erro no servidor, tente mais tarde.'));
+        .subscribe((category) => {
+          this.category = category
+          console.log(category)
+          this.categoryForm.patchValue(category) // setando os valores da categoria, para o formulário
+        }, (error) => alert('Ocorreu um erro no servidor, tente mais tarde.'));
     }
   }
-
 
   // Seta o titulo da página de acordo com a ação em questao
-  private setPageTitle(){
-    if(this.currentAction == 'new'){
-      this.pageTitle = 'Cadastro de Nova Categoria' // se tiver cadastrando, exiba esse titulo
+  private setPageTitle() {
+    if (this.currentAction == 'new') {
+      this.pageTitle = "Cadastro de Nova Categoria" // se tiver cadastrando, exiba esse titulo
     }
-    else{
+    else {
       const categoryName = this.category.name || "" // garante que ao começar o ciclo de vida do componente, tenha uma categoria já carregada
-      this.pageTitle = `Editando Categoria ${categoryName}` // se tiver editando, exiba esse titulo
+      this.pageTitle = "Editando categoria: " + categoryName // se tiver editando, exiba esse titulo
     }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value) // monta um objeto "category", de acordo com oque tem no formulário
+
+    this.categoryService.create(category)// envia a categoia para o servidor, para ser criado uma categoria nova
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+
+  }
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value) // cria um objeto category preenchido de acordo com os dados que estão no formulário.
+
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+        )
+  }
+
+  private actionsForSuccess(category?: Category) {
+
+    toastr.success("Solicitação processada com sucesso!")
+
+    // redirecinamento/reload da pagina do componente
+    this.router.navigateByUrl("categories", { skipLocationChange: true }).then(
+      () => this.router.navigate(["categories", category.id, "edit"]))// permanece na pagina de cadastro, porem com recurso de edição, já coms os dados cadastrados prontos para editar
+    console.log(category)
+
 
   }
 
+  private actionsForError(error) {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!")
+
+    this.submittingForm = false; // desativa o btn de enviar
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).erros;
+    }
+    else {
+      this.serverErrorMessages = ["Falha na cominicação com o servidor. Por favor, tente mais tarde."]
+    }
+  }
 }
